18420546d45c403728bd29837bf701132062337b
Philip Yoo <philipyoo10@gmail.com>
2017-02-24 22:53:52 -0800

    Update app.js, add script on html

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+require('aframe');
 var ipc = require('electron').ipcRenderer;
+
+var el = document.querySelector("#getthis");
+
+// AFRAME.registerComponent('position', {
+//   schema: { type: 'vec3' },
+
+//   update: function() {
+//     var object3D = this.el.object3D;
+//     var data = this.data;
+//     object3D.position.set(data.x, data.y, data.z);
+//   }
+// });
