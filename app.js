b1a55730411b11f2e485b94095decff7964f9201
Philip Yoo <philipyoo10@gmail.com>
2017-02-25 13:23:40 -0800

    Fix messages sent between front and back js

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const AFRAME = window.AFRAME;
 
 var tester = document.querySelector('#testme');
 
+
 tester.addEventListener('click', function() {
-  // ipc.once('addContainerReceive', function(res) {
-  //   console.log("Got response back");
-  //   console.log(res);
-  // })
+  ipc.once('addContainerReceive', function(event, res) {
+    console.log("Got response back");
+    console.log(res);
+    // Dynamically create object here
+  })
 
   this.setAttribute('material', 'color', 'red');
-  console.log("I was clicked");
 
-  // var name = "IRMADE";
+  var name = document.getElementById('cname');
 
-  // ipc.send('addContainerSend', name);
+  ipc.send('addContainerSend', name.value);
 });
 
