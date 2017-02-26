529b963f4283999d0e49d62776376654a1743388
Philip Yoo <philipyoo10@gmail.com>
2017-02-25 17:34:53 -0800

    Add box show function

--- app.js
+++ app.js
@@ -2,23 +2,6 @@ var ipc = require('electron').ipcRenderer;
 const AFRAME = window.AFRAME;
 
 
-// var tester = document.querySelector('#testme');
-
-
-// tester.addEventListener('click', function() {
-//   ipc.once('addContainerReceive', function(event, res) {
-//     console.log("Got response back");
-//     console.log(res);
-//     // Dynamically create object here
-//   })
-
-//   this.setAttribute('material', 'color', 'red');
-
-//   var name = document.getElementById('cname');
-
-//   ipc.send('addContainerSend', name.value);
-// });
-
 var cr = document.querySelector('#create-sky');
 
 var t1 = document.querySelector('#box1');
@@ -35,13 +18,18 @@ var t11 = document.querySelector('#box11');
 var t12 = document.querySelector('#box12');
 
 var containers = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12];
+shuffle(containers);
 
 cr.addEventListener('click', function(evt) {
   console.log("creating container..");
   ipc.once('addContainerReceive', function(event, res) {
     console.log("Got response back");
     console.log(res);
+
+    show_container = containers.shift();
+
     document.getElementById('cname').value = "";
+    show_container.setAttribute("height", 1);
   });
 
   var name = document.getElementById('cname');
@@ -52,59 +40,81 @@ cr.addEventListener('click', function(evt) {
 t1.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t2.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t3.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t4.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t5.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t6.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t7.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t8.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t9.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t10.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t11.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
 
 t12.addEventListener('click', function(evt) {
   console.log("inside");
   console.log(evt);
+  console.log(this);
 });
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
